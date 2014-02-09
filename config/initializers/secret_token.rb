446bd1623e51fe9cf90f266b14a989c87cd6036e
Anton Zhavoronkov <anton.zhavoronkov@gmail.com>
2014-02-09 14:05:16 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JrubyTest::Application.config.secret_key_base = '8ed40a0be4e39307ffd7ace734d29f6d2975dbc6634b72502454e1561cba01972ca806f99b7330ff5a90671d3902937a7aef17fe8f98d26ee778f985e2cf3f0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JrubyTest::Application.config.session_store :cookie_store, key: '_jruby_test_session'
